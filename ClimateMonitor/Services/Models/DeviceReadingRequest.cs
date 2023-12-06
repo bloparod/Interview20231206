34a2511693b6846bd44868e47bf28218fd5ecbcf
Pablo Rodríguez <bloparod@gmail.com>
2023-12-06 18:31:00 -0300

    Solved Issue 2 (unit tests ReturnErrorWhenInvalidFirmware). Added SemVer regex validation to DeviceReadingRequest.FirmwareVersion.

--- ClimateMonitor/Services/Models/DeviceReadingRequest.cs
+++ ClimateMonitor/Services/Models/DeviceReadingRequest.cs
@@ -1,10 +1,18 @@
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace ClimateMonitor.Services.Models;
 
 public class DeviceReadingRequest
 {
+    private const string pattern = @"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$";
+
     [Required]
+    [RegularExpression(
+        pattern,
+        ErrorMessage = "The firmware value does not match semantic versioning format."
+    )]
     public string FirmwareVersion { get; set; } = string.Empty;
 
     [Required]
